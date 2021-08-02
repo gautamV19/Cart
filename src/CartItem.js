f19a92796fb1cfc448e00a14cb6215989347998d
gautamVaja <gautamiitmee@gmail.com>
2021-08-02 09:18:45 +0530

    made first cart item

--- src/CartItem.js
+++ src/CartItem.js
@@ -13,6 +13,9 @@ class CartItem extends React.Component {
                     <div style={{ color: '#777' }}>Qty: 1</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img alt='increase' className='action-icons' src='https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg' />
+                        <img alt='decrease' className='action-icons' src='https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg' />
+                        <img alt='delete' className='action-icons' src='https://as2.ftcdn.net/v2/jpg/01/90/89/15/1000_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg' />
                     </div>
                 </div>
             </div>
@@ -25,7 +28,7 @@ const styles = {
         height: 110,
         width: 110,
         borderRadius: 4,
-        background: '#ccc' 
+        background: '#ccc'
     }
 }
 
