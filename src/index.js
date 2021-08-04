5f07fb1361ab037445af8cad51292d950d9a5ed6
gautamVaja <gautamiitmee@gmail.com>
2021-08-04 11:41:55 +0530

    add firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import * as serviceWorker from './serviceWorker';
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAaHbwonMEZaRVwS4HIFV1licsSZBbiBSs",
+  authDomain: "cart-66397.firebaseapp.com",
+  projectId: "cart-66397",
+  storageBucket: "cart-66397.appspot.com",
+  messagingSenderId: "679343662364",
+  appId: "1:679343662364:web:ad50c46d958c8b2a6d06d9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,3 +25,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// serviceWorker.unregister();
